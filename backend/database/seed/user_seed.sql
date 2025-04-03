ca610861e5e25271ad2999bcc36450c3da9c9fe9
clee3013 <clee3013@gatech.edu>
2025-04-03 17:05:37 -0400

    Updated seed file

---
+++ backend/database/seed/user_seed.sql
@@ -0,0 +1,54 @@
+USE cs6400_sp25_team001;
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('adinos@ganjaflex.com', 'pwd', 'Markus', 'Chavous', '1980-10-19', '3749016371');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('ahoa@j-texon.com', 'pwd', 'Mireya', 'Matuszak', '2013-02-01', '7725204976');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('alabreche@konex.com', 'pwd', 'Cyndy', 'Kardas', '2010-03-18', '6947062121');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('asturiale@hottechi.com', 'pwd', 'Erick', 'Sawchuk', '2013-07-03', '4435428566');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('atawil@golddex.com', 'pwd', 'Chantell', 'Mcdoniel', '1983-12-21', '810719344');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('bkarpel@plexzap.com', 'pwd', 'Angella', 'Meisel', '1965-08-24', '1146220083');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('bstimmel@hatfan.com', 'pwd', 'Laticia', 'Aquas', '2008-08-29', '4215775416');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('cfallick@zoomit.com', 'pwd', 'Beatriz', 'Eanes', '2010-08-20', '3767348918');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('ddegonia@finjob.com', 'pwd', 'Ammie', 'Denooyer', '1964-08-23', '4172571031');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('ebayless@streethex.com', 'pwd', 'Deandrea', 'Fern', '1989-09-19', '9990929503');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('execdir@burdell.com', 'execdir', 'George', 'Burdell', '1995-06-01', '1878031751');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('fperigo@condax.com', 'pwd', 'Graciela', 'Sarao', '1986-08-09', '5238087854');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('fscheyer@cancity.com', 'pwd', 'Haydee', 'Tomblin', '1966-08-28', '8678533989');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('kbenimadho@newex.com', 'pwd', 'Oretha', 'Ostrosky', '1974-09-18', '4188266984');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('kscriven@betasoloin.com', 'pwd', 'Andra', 'Sama', '1984-09-19', '6920519235');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('kyum@goodsilron.com', 'pwd', 'Cyril', 'Samu', '1971-08-26', '5131693393');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('ljillson@rangreen.com', 'pwd', 'Erick', 'Vizarro', '2011-10-07', '6850096323');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('lstaback@conecom.com', 'pwd', 'Erick', 'Abdallah', '1973-05-24', '1809078612');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('mdiestel@hatfan.com', 'pwd', 'Garry', 'Tolfree', '1991-07-27', '1676258250');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('mhagele@finjob.com', 'pwd', 'Gail', 'Paprocki', '1997-12-27', '8708333983');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('mrowling@finhigh.com', 'pwd', 'Stephaine', 'Albares', '1994-11-04', '1990488763');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('shalter@treequote.com', 'pwd', 'Rolland', 'Dallen', '1993-08-21', '3205334461');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('skalafatis@xx-holding.com', 'pwd', 'Latrice', 'Suffield', '1975-01-15', '4463962875');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('smadarang@zotware.com', 'pwd', 'France', 'Buemi', '1966-05-21', '7266115255');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('spawlowicz@donware.com', 'pwd', 'Kasandra', 'Jurney', '1974-11-01', '7277642546');
+INSERT INTO User (email, password, first_name, last_name, birthdate, phone_number)
+VALUES ('tcastros@hatfan.com', 'pwd', 'Twana', 'Nabours', '2007-06-08', '5834716716');
+INSERT INTO ExecutiveDirector (email) VALUES ('execdir@burdell.com');
